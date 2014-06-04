8ba7a9b51ebcd3826f838c1491a51cdf20646f7f
AdamSimpson <adambsimpson@gmail.com>
2014-06-04 16:15:40 +0000

    Adding post config file

---
+++ pi_post_setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+touch ~/mpihostfile
+
+for i in {1..3}
+do
+    num=$(($i+100))
+    ip=192.168.3.$num
+    echo $ip >> ~/mpihostfile
+    sudo sh -c "echo \"$ip	pi$i\" >> /etc/hosts"
+    sshpass -p 'raspberry' scp -o StrictHostKeyChecking=no pi@pi$i:~/.ssh/id_rsa.pub tmp_key
+    cat tmp_key >> ~/.ssh/authorized_keys
+done
+
+for i in {1..3}
+do
+    sshpass -p 'raspberry' scp -o StrictHostKeyChecking=no ~/.ssh/authorized_keys pi@pi$i:~/.ssh/authorized_keys
+done
+
+sudo rm tmp_key
