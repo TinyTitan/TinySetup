a476002ea519c9f27d84d419e8c341adc97d5fa2
mjdonovan410 <mjdonovan410@gmail.com>
2014-08-12 17:09:40 -0400

    Added libraries for gravity experiment

--- pi_setup.sh
+++ pi_setup.sh
@@ -1,5 +1,28 @@
 #!/usr/bin/env bash
 
+#The MIT License (MIT)
+
+#Copyright (c) 2014 Oak Ridge National Laboratory
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+
+
 echo "Enter the node number of this pi followed by [ENTER]: "
 read pi_number
 re='^[0-9]+$'
@@ -11,7 +34,15 @@ pi_name="pi$pi_number"
 echo "Installing system software and updates"
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install vim mpich2 xboxdrv libglew-dev sshpass python-mpi4py libav-tools
+sudo apt-get -y install vim mpich2 xboxdrv libglew-dev sshpass libav-tools
+sudo apt-get -y install python-mpi4py python-six python-dateutil python-matplotlib
+
+curLoc = "$PWD"
+cd /home/pi
+git clone https://github.com/richardghirst/PiBits.git
+cd ./PiBits/ServoBlaster/user
+make
+cd $curLoc
 
 echo "Setting computer name"
 for file in \
